b05f9c30bd5a0c3466ebd8b6b5c3e416c5061080
Amanda Haldy <ahaldy@ebsco.com>
2024-08-01 15:36:12 -0400

    Adds search result models and stubs for landing and search pages

---
+++ src/services/models/searchResult.ts
@@ -0,0 +1,14 @@
+export type SearchResult = {
+  title: string;
+  author_name: string;
+  first_publish_year: number;
+  number_of_pages_median: number;
+  subject: string[];
+  ratings_average: number;
+  currently_reading_count: number;
+  cover_i: number;
+}
+
+export type SearchResponse = {
+  docs: SearchResult[];
+}
